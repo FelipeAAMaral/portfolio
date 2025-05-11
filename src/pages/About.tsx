4955e55759cb24457a8fc5055e13b55478849b9e
Felipe Amaral <amaral.felipeaugusto@gmail.com>
2025-05-11 14:30:03 -0300

    .

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,14 +8,14 @@ import SkillsToolbox from "@/components/about/SkillsToolbox";
 import CpoPath from "@/components/about/CpoPath";
 import AboutMe from "@/components/about/AboutMe";
 import BuildTogether from "@/components/about/BuildTogether";
-import { convertCFToMedium } from '@/lib/utils';    
+// import { convertCFToMedium } from '@/lib/utils';    
 import type { MediumPost }       from '@/lib/useMediumPosts';
 import { useMediumPosts } from '@/lib/useMediumPosts';
 import type { CFPost } from '@/lib/useContentfulPosts';
   
 const About = () => {
   const { t } = useLanguage();
-  const mediumPosts = convertCFToMedium(useMediumPosts().posts as CFPost[]);
+  // const mediumPosts = convertCFToMedium(useMediumPosts().posts as CFPost[]);
 
   return (
     <div className="container py-16 animate-fade-in">
